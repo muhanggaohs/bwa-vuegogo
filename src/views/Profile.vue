dbdb62061a28c48374ef23c4e1f4363324179336
Angga <angga.oktaharisetia@gmail.com>
2020-12-30 23:10:04 +0700

    init

---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>This is a profile page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Profile',
+  components: {
+
+  }
+}
+</script>
